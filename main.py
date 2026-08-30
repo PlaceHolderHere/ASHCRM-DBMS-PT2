e08a1dbfaa398c9a14ade2921b09eb6e327bb4dc
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-30 14:02:11 +0800

    Added a function to trigger every time the screen size is changed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import tkinter
+
 from dotenv import load_dotenv
 import os
 import mysql.connector
@@ -30,14 +32,16 @@ class App:
         self.window_height: int = 720
         self.viewWidth = self.window_width / 100
         self.viewHeight = self.window_height / 100
+        self._resize_timer = None
 
         # Tkinter Initialization
         self.root = tk.Tk()
         self.root.title("ASHCRM")
         self.center_window()
 
-        # Run a function when the user closes the window
-        self.root.protocol("WM_DELETE_WINDOW", self.close_app)
+        # Run a function when the user:
+        self.root.protocol("WM_DELETE_WINDOW", self.close_app) # Closes the window
+        self.root.bind("<Configure>", self.on_configure) # Resizing the window
 
     def start_app(self):
         self.root.mainloop()
@@ -47,6 +51,28 @@ class App:
             self.cursor.close()
             self.connection.close()
             self.root.destroy()  # close the window
+
+    def on_configure(self, event: tkinter.Event) -> None:
+        if event.widget != self.root:
+            return
+
+        # Checking if the window has been resized
+        new_window_width: int = event.width
+        new_window_height: int = event.height
+        if new_window_width == self.window_width and self.window_height == new_window_height:
+            return
+
+        # Runs on_resize() if the window hasnt been resized after 250ms
+        if self._resize_timer is not None:
+            self.root.after_cancel(self._resize_timer)
+
+        self._resize_timer = self.root.after(250, lambda : self.on_resize(new_window_width, new_window_height))
+
+    def on_resize(self, window_width: int, window_height: int) -> None:
+        self.window_width = window_width
+        self.window_height = window_height
+        self.viewWidth = self.window_width / 100
+        self.viewHeight = self.window_height / 100
 
     def center_window(self):
         self.root.update_idletasks()
