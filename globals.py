506060c0ab1c13226f6ff57c9cffabd3efa938ec
PlaceHolderHere <holderhereinplace@gmail.com>
2026-09-10 10:45:20 +0800

    Added brand colors to globals.py

--- globals.py
+++ globals.py
@@ -5,7 +5,12 @@ from pages.login_page import LogInPage
 from pages.students_page import StudentsPage
 from qr_scanner import QrScanner
 
-# List of all global variables that can be accessed by all python files
+# List of all global variables and constants that can be accessed by all python files
+PRIMARY_COLOR = "#aecfe4"
+ACCENT_COLOR = "#46748e"
+ACCENT_DARK = "#335e76"
+BACKGROUND_COLOR = "#ffffff"
+HEADER_COLOR = "#627e8f"
 window_width: int = 1280
 window_height: int = 720
 viewWidth: float = window_width / 100
